cc8e2aa75d434dcd7addcf1e3dfe7197dca3190e
Lucas Costa <lucasrc.rodri@gmail.com>
2023-03-11 12:38:30 -0300

    fix

--- Aula02/Atividade01.py
+++ Aula02/Atividade01.py
@@ -1,10 +1,10 @@
 #Categorias:
-"""
+""" Enunciado correto:
     Menos que 0º é congelante,
     De 0º a 10º é muito frio,
-    De 10,1º a 17º é friozinho,
-    De 17,1º a 24º é ameno,
-    De 24,1º a 30º é calor,
+    Aicma 10º a 17º é friozinho,
+    Acima 17º a 24º é ameno,
+    Acima 24º a 30º é calor,
     Acima de 30º é muito calor.
 """
 #Usuário informa a temperatura
@@ -14,11 +14,11 @@ temperatura = float(temperatura)
 #Retornando ao usuário a informação sobre a temperatura de acordo com a tabela:
 if temperatura < 0:
     print("Hoje tá congelante.")
-elif temperatura < 10.1:
+elif temperatura <= 10:
     print("Tá um muito frio hoje!")
-elif temperatura < 17.1:
+elif temperatura <= 17:
     print("Hoja tá fazendo um friozinho.")
-elif temperatura < 24.1:
+elif temperatura <= 24:
     print("Hoja tá bem ameno.")
 elif temperatura <= 30:
     print("Tá um calor hoje!!!")
